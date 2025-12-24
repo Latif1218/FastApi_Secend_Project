67bdd26d221ed10c298c21a3c591ea5a5de3162a
Latif1218 <mdsabdullotif@gmail.com>
2025-12-24 17:03:33 +0600

    add

---
+++ app/config.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY", "JWT_SECRET_KEY")
+
+
+
+
+EMAIL_HOST = os.getenv("EMAIL_HOST", "EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "EMAIL_PORT"))  
+EMAIL_USER = os.getenv("EMAIL_USER")           
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")   
+EMAIL_FROM = os.getenv("EMAIL_FROM", EMAIL_USER)
+EMAIL_USE_TLS = True
+
+
+
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/google/callback")
+GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
+GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo"
---
+++ app/utils/email_sender.py
@@ -0,0 +1,4 @@
+import smtplib
+from fastapi import HTTPException, status
+from email.message import EmailMessage
+from ..
